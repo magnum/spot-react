26f52634a1bcd1b133044c684a3f893223d6635b
Emanuele DelBono <emanuele@codiceplastico.com>
2015-09-16 16:18:27 +0200

    sass with separate file (only in production)

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,11 +1,14 @@
 var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: [
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/dev-server',
       path.resolve(__dirname, 'src/components/App.jsx'),
       './src/index.html'
+    ],
+    plugins: [
+      new ExtractTextPlugin('app.css', {allChunks: true})
     ],
     output: {
         filename: '/js/app.js',
@@ -17,14 +20,15 @@ module.exports = {
     module: {
       loaders: [{
         test: /\.html$/,
-        loaders: ['file?name=[name].[ext]'],
+        loaders: ["file?name=[name].[ext]"],
       },{
         test: /\.jsx?$/,
-        loaders: ['babel', 'react-hot'], 
+        loaders: ['babel'], 
         include: path.join(__dirname, 'src')
       },{
         test: /\.scss$/,
-        loader: 'style!css!sass'
+        loader: ExtractTextPlugin.extract('style', 'css!sass')
+
       },{
         test: /\.(woff|woff2|eot|svg|ttf)$/,
         loader: 'url?limit=10000000'
