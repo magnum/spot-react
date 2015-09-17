49ac362c74d91f788a961fff801e34eef9ac8556
Emanuele DelBono <emanuele@codiceplastico.com>
2015-09-17 11:46:44 +0200

    added enabled/disable support

--- src/components/search/SearchBox.jsx
+++ src/components/search/SearchBox.jsx
@@ -1,19 +1,26 @@
 var React = require('react');
 
 var SearchBox = React.createClass({
+  getInitialState: function() {
+    return { value: '' };
+  },
   onSearch: function(){
-    var artist = this.refs.artist.getDOMNode().value;
+    var artist = this.state.value;
     if (typeof this.props.onSearchCallback == 'function'){
       this.props.onSearchCallback(artist);
     }
   },
+  onChange: function(event) {
+    this.setState({value: event.target.value});
+  },
   render: function() {
+    var isDisabled = this.state.value == '';
     return (
       <form>
         <div className="form-group col-xs-10">
-          <input className="form-control" type="text" ref="artist" />
+          <input className="form-control" type="text" value={this.state.value} onChange={this.onChange}/>
         </div>
-        <button className="btn btn-primary" onClick={this.onSearch}><i className="fa fa-search"></i></button>
+        <button className="btn btn-primary" disabled={isDisabled} onClick={this.onSearch}><i className="fa fa-search"></i></button>
       </form>
     );
   }
