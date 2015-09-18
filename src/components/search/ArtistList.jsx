b95a36191477a5d2403a8dc11efb9ad5a5db2bfc
Emanuele DelBono <emanuele@codiceplastico.com>
2015-09-18 20:50:45 +0200

    refactoring to es6

--- src/components/search/ArtistList.jsx
+++ src/components/search/ArtistList.jsx
@@ -3,7 +3,7 @@ import Artist from './Artist';
 
 export default class ArtistList extends React.Component{
   render() {
-    let artists = this.props.list.map(function(a){
+    let artists = this.props.list.map(a=>{
       return <li key={a.id}><Artist info={a} /></li>
     });
     return (
