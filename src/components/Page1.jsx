c01077df959b501dca2c405156923c2a3d02ae96
Emanuele DelBono <emanuele@codiceplastico.com>
2015-09-19 14:16:47 +0200

    basic sample

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,12 +1,46 @@
 var React = require('react');
 
 var Home = React.createClass({
+  getInitialState: function() {
+    return {
+      counter:0,
+      clicks: []
+    };
+  },
+  handleClick: function(){
+    this.state.clicks.push(new Date());
+    this.setState({counter: this.state.counter + 1, clicks: this.state.clicks});
+  },
+  onChange: function(evt){
+    this.setState({counter: parseInt(evt.target.value)});
+  },
   render: function() {
+    var clicks = this.state.clicks.map(function(c, i) { return <DateItem key={i} value={c} /> });
     return (
       <div>
-        Page one
+        <input type="text" value={this.state.counter} onChange={this.onChange} />
+        <MyButton label="Click here" clickCallback={this.handleClick} />
+        <ul>
+          {clicks}
+        </ul>
       </div>
       );
+  }
+});
+
+var DateItem = React.createClass({
+  render: function(){
+    var date = this.props.value;
+    return <li>{date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()}</li>
+  }
+});
+
+var MyButton = React.createClass({
+  handleOnClick: function(){
+    this.props.clickCallback();
+  },
+  render: function(){
+    return <button type="button" onClick={this.handleOnClick} >{this.props.label}</button>
   }
 });
 
